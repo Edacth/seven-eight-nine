116d14bd56b6f326999f59cab0a35de9a7215327
HallCooper <48073300+HallCooper@users.noreply.github.com>
2020-01-23 09:21:53 -0800

    Basic AI Cooper
    v1

---
+++ Assets/Scripts/CreatureAttackAIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreatureAttackAIScript : MonoBehaviour
+{
+
+    private NavMeshAgent CreatureCapsule;
+
+    public GameObject PlayerCapsule;
+
+    public float CreatureAttackDistance = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatureCapsule = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, PlayerCapsule.transform.position);
+
+        if (distance < CreatureAttackDistance)
+        {
+            Vector3 dirToPlayer = transform.position - PlayerCapsule.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            CreatureCapsule.SetDestination(newPos);
+        }
+    }
+}
